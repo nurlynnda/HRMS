25853d9b65de97a8d943bbdb2c81ebcbb270cc47
Lynnda <n.lynnda65@gmail.com>
2026-07-20 13:09:56 +0800

    Add placeholder Home/Attendance/Leave/Profile tab screens

---
+++ hrms_app/lib/screens/leave/leave_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class LeaveScreen extends StatelessWidget {
+  const LeaveScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Leave', style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700)),
+    );
+  }
+}
